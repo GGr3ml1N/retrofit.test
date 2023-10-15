6ce8b348bcf7a97cdb0b46f6b7a1f12293e10d25
Dinar Subeev <dinars163@gmail.com>
2023-10-16 01:11:15 +0400

    Binding, vm connected. Test request

--- app/src/main/java/com/ggr3ml1n/retrofittest/MainActivity.kt
+++ app/src/main/java/com/ggr3ml1n/retrofittest/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.ggr3ml1n.retrofittest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.viewModels
+import com.ggr3ml1n.retrofittest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+    private val vm: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        vm.productLiveData.observe(this) {
+            binding.tv.text = it.title
+        }
+
+        binding.b.setOnClickListener {
+            vm.initialize()
+        }
     }
 }
